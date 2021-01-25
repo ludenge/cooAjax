1d52e792add1b990547cbdd113567448eef28d4e
liqn <liqn@wedoctor.com>
2021-01-25 15:32:24 +0800

    add: fetch

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
 # cooAjax
 cooAjax 用于拦截ajax。 它细化原生ajax请求过程，包括beforeOpen, afterOpen, beforeSend, afterSend, loadend, onreadystatechange六个生命周期钩子
 
+如果需要对fetch API的请求拦截, cooAjax提供两个钩子函数：beforeRequest, beforeResponse
+
 ## 使用
 
 ```js
@@ -42,7 +44,26 @@ cooAjax 用于拦截ajax。 它细化原生ajax请求过程，包括beforeOpen, 
   cooAjax.stop()
 ```
 
-## 示例
+```js
+  # fetch拦截
+  cooAjax.start({
+    # 参数 args为 fetch() 参数
+    beforeRequest(args) {
+      this.req = args
+      console.log('before Request', args)
+    },
+    # 参数 response为原响应对象
+    -- 如果想根据 url 拦截响应对象，请先在 beforeRequest 里手动将 args 保存到变量中
+    -- beforeResponse 返回值（不为false）即为响应值。返回值最好返回字符串，返回非字符串时，cooAjax内部会调用JJSON.stringify()方法进行转换
+    beforeResponse(response) {
+      console.log(this.req)
+      console.log('before Response', response)
+      return { name: '拦截响应值' }
+    }
+  })
+```
+
+## 示例1
 ```js
   cooAjax.start({
     beforeOpen(args) {
@@ -52,6 +73,7 @@ cooAjax 用于拦截ajax。 它细化原生ajax请求过程，包括beforeOpen, 
     },
     afterOpen(args, xhr) {
       cooAjax.set(xhr, 'response', JSON.stringify({ value: '我可能是来自loacalstorage的数据'}))
+      cooAjax.set(xhr, 'responseText', JSON.stringify({ value: '我可能是来自loacalstorage的数据'}))
     }
   })
   
@@ -67,4 +89,46 @@ cooAjax 用于拦截ajax。 它细化原生ajax请求过程，包括beforeOpen, 
 
 我们用原生方式，请求*./list.json*，经过 cooAjax 的处理，实际网络请求url是 *./xxx/list.json*。并且，打印 response 的时候，得到的值为 *{"value":"我可能是来自loacalstorage的数据"}*
 
-通过一个简单的例子，我们实现 对 ajax 的拦截，修改了请求url和响应数据，虽然 cooAjax 代码简单，但功能强大，欢迎一起发掘它更多好玩的用法。
+通过一个简单的例子，我们实现 对 ajax 的拦截，修改了请求url和响应数据。
+
+## 示例2
+```js
+  cooAjax.start({
+    beforeRequest(args) {
+      // 添加请求头
+      if (args[1].headers) {
+        args[1].headers['x-cur-ajax'] = 'cooAjax'
+      } else {
+        args[1].headers = {
+          ['x-cur-ajax']: 'cooAjax'
+        }
+      }
+      this.req = args
+      console.log('before Request', args)
+    },
+    beforeResponse(response) {
+      console.log('before Response', response)
+      if (this.req[0].includes('/news')) {
+        return {
+          name: '蓝上连牙 蓝上了！'
+        }
+      }
+    }
+  })
+  
+  fetch('./news.json', {
+    method: 'get'
+  }).then(response => {
+    return response.text()
+  }).then(data => {
+    console.log('data', data)
+  })
+```
+
+介于fetch API本身特点，cooAjax提供beforeRequest、beforeResponse两个钩子在**请求之前**和**响应之前**进行拦截。
+
+如上例，请求头会加上我们自定义的字段（只是示例，非必要，请根据实际情况选择是否添加），当请求url包含'/news'时，响应数据为 '{ "name": "蓝上连牙 蓝上了！"}'
+
+-----------------------------------------------------------
+
+虽然 cooAjax 代码简单，但功能强大，欢迎一起发掘它更多好玩的用法。喜欢的话，请赏个赞呗
--- cooAjax.js
+++ cooAjax.js
@@ -1,30 +1,71 @@
 window.cooAjax = {
   XHR: window.XMLHttpRequest,
+  fetch: window.fetch.bind(window),
+  opts: {},
   start(opts) {
-    return window.XMLHttpRequest = class extends cooAjax.XHR {
-      constructor() {
-        super()
-        opts['loadend'] && this.addEventListener('loadend', opts['loadend'])
-        opts['onreadystatechange'] && this.addEventListener('readystatechange', opts['onreadystatechange'])
-      }
-      open() {
-        opts['beforeOpen'] && opts['beforeOpen'](arguments)
-        cooAjax.XHR.prototype['open'].apply(this, arguments)
-        opts['afterOpen'] && opts['afterOpen'](arguments, this)
-      }
-      send() {
-        opts['beforeSend'] && opts['beforeSend'](arguments) || cooAjax.XHR.prototype['send'].apply(this,
-          arguments)
-        opts['afterSend'] && opts['afterSend'](arguments, this)
-      }
+    Object.assign(this.opts, opts)
+    window.XMLHttpRequest = cooenXHR
+    if (opts['beforeRequest'] || opts['beforeResponse']) {
+      window.fetch = cooenFetch
     }
   },
   stop() {
-    window.XMLHttpRequest = cooAjax.XHR;
+    this.opts = {}
+    window.XMLHttpRequest = cooAjax.XHR
+    if (window.fetch === cooenFetch) {
+      window.fetch = cooAjax.fetch
+    }
   },
   set(obj, prop, value) {
     Object.defineProperty(obj, prop, {
       get: () => value
     })
   }
+}
+
+class cooenXHR extends cooAjax.XHR {
+  constructor() {
+    super()
+    cooAjax.opts['loadend'] && this.addEventListener('loadend', cooAjax.opts['loadend'])
+    cooAjax.opts['onreadystatechange'] && this.addEventListener('readystatechange', cooAjax.opts['onreadystatechange'])
+  }
+  open() {
+    cooAjax.opts['beforeOpen'] && cooAjax.opts['beforeOpen'](arguments)
+    cooAjax.XHR.prototype['open'].apply(this, arguments)
+    cooAjax.opts['afterOpen'] && cooAjax.opts['afterOpen'](arguments, this)
+  }
+  send() {
+    cooAjax.opts['beforeSend'] && cooAjax.opts['beforeSend'](arguments) || cooAjax.XHR.prototype['send'].apply(this,
+      arguments)
+    cooAjax.opts['afterSend'] && cooAjax.opts['afterSend'](arguments, this)
+  }
+}
+
+const cooenFetch = function() {
+  // fetch(url, init)形式可以拦截， fetch(new Request)不拦截
+  typeof arguments[0] === 'string' && cooAjax.opts['beforeRequest'] && cooAjax.opts['beforeRequest'](arguments)
+  return cooAjax.fetch(...arguments).then((response) => {
+    let txt = cooAjax.opts['beforeResponse'] && cooAjax.opts['beforeResponse'](response)
+    if (!txt) return response
+    
+    if (typeof txt !== 'string') {
+      try{
+        txt = JSON.stringify(txt)
+      }catch(e){
+        console.log(e)
+        return response
+      }
+    }
+    const proxy = new Proxy(new Response(txt), {
+      get: function (target, name) {
+        if(typeof target[name] === 'function') {
+          if (name === 'clone') return response[name].bind(response)
+          // arrayBuffer blob json text formData
+          return target[name].bind(target)
+        }
+        return response[name]
+      }
+    })
+    return proxy
+  })
 }
